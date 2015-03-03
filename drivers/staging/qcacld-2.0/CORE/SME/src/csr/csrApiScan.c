973288e11576925dbc09efee7a21e1322d2b41c0
AnjaneeDevi Kapparapu <akappa@codeaurora.org>
2015-03-02 16:22:13 -0800

    qcacld-2.0: Fix memory leak for 3 port concurrency feature.
    This fix will resolve memory leak issue where memory was not
    getting released through calling csrFreeScanFilter.
    
    CRs-Fixed: 802042
    Change-Id: I5c20fc66add1903f0ea39be663a239e24cfb180d

--- drivers/staging/qcacld-2.0/CORE/SME/src/csr/csrApiScan.c
+++ drivers/staging/qcacld-2.0/CORE/SME/src/csr/csrApiScan.c
@@ -7507,6 +7507,7 @@ eHalStatus csrScanGetBKIDCandidateList(tpAniSirGlobal pMac, tANI_U32 sessionId,
                     }
                     csrScanResultPurge(pMac, hBSSList);
                 }//Have scan result
+                csrFreeScanFilter(pMac, pScanFilter);
             }
             vos_mem_free(pScanFilter);
         }
