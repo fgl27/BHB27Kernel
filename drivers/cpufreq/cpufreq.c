7b07a99783ecb16bb9990a70ee82ea19630a6d53
bhb27 <fella.motog@gmail.com>
2016-02-02 12:46:14 -0200

    Fix cpufreq.c warning: 'show_cpu_utilization'
    drivers/cpufreq/cpufreq.c:416:16: warning: 'show_cpu_utilization' defined but not used [-Wunused-function]
    error, forbidden warning: cpufreq.c:
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -644,6 +644,8 @@ cpufreq_freq_attr_ro(scaling_cur_freq);
 cpufreq_freq_attr_ro(bios_limit);
 cpufreq_freq_attr_ro(related_cpus);
 cpufreq_freq_attr_ro(affected_cpus);
+cpufreq_freq_attr_ro(cpu_utilization);
+cpufreq_freq_attr_ro(cpu_load);
 cpufreq_freq_attr_rw(scaling_min_freq);
 cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
@@ -659,6 +661,8 @@ static struct attribute *default_attrs[] = {
 	&cpuinfo_transition_latency.attr,
 	&scaling_min_freq.attr,
 	&scaling_max_freq.attr,
+	&cpu_utilization.attr,
+	&cpu_load.attr,
 	&affected_cpus.attr,
 	&related_cpus.attr,
 	&scaling_governor.attr,
--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -310,6 +310,9 @@ int cpufreq_unregister_notifier(struct notifier_block *nb, unsigned int list);
 void cpufreq_notify_transition(struct cpufreq_policy *policy,
 		struct cpufreq_freqs *freqs, unsigned int state);
 
+void cpufreq_notify_utilization(struct cpufreq_policy *policy,
+                                unsigned int load);
+
 #else /* CONFIG_CPU_FREQ */
 static inline int cpufreq_register_notifier(struct notifier_block *nb,
 						unsigned int list)
