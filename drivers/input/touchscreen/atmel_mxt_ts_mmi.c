11234fafb662da9e20e09aa05669206f71ca4a07
Pranav Vashi <neobuddy89@gmail.com>
2016-03-20 12:23:13 +0530

    shamu: Add state notifier driver
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- drivers/input/touchscreen/atmel_mxt_ts_mmi.c
+++ drivers/input/touchscreen/atmel_mxt_ts_mmi.c
@@ -39,6 +39,10 @@
 #include <linux/wake_gestures.h>
 #endif
 
+#ifdef CONFIG_STATE_NOTIFIER
+#include <linux/state_notifier.h>
+#endif
+
 enum {
 	STATE_UNKNOWN,
 	STATE_ACTIVE,
@@ -2553,7 +2557,7 @@ static void mxt_set_sensor_state(struct mxt_data *data, int state)
 	case STATE_UNKNOWN:
 	case STATE_FLASH:
 		/* no special handling for these states */
-			break;
+		break;
 
 	case STATE_SUSPEND:
 		if (!data->mode_is_wakeable)
@@ -2561,7 +2565,10 @@ static void mxt_set_sensor_state(struct mxt_data *data, int state)
 		data->enable_reporting = false;
 		if (!data->in_bootloader)
 			mxt_sensor_state_config(data, SUSPEND_IDX);
-			break;
+#ifdef CONFIG_STATE_NOTIFIER
+		state_suspend();
+#endif
+		break;
 
 #ifdef CONFIG_WAKE_GESTURES
 	case STATE_WG:
@@ -2583,24 +2590,27 @@ static void mxt_set_sensor_state(struct mxt_data *data, int state)
 			mxt_restore_default_mode(data);
 			pr_debug("Non-persistent mode; restoring default\n");
 		}
-			break;
+#ifdef CONFIG_STATE_NOTIFIER
+		state_resume();
+#endif
+		break;
 
 	case STATE_STANDBY:
 		mxt_irq_enable(data, false);
-			break;
+		break;
 
 	case STATE_BL:
 		if (!data->in_bootloader)
 			data->in_bootloader = true;
 
 		mxt_irq_enable(data, false);
-			break;
+		break;
 
 	case STATE_INIT:
 		/* set flag to avoid object specific message handling */
 		if (!data->in_bootloader)
 			data->in_bootloader = true;
-			break;
+		break;
 	}
 
 	pr_info("state change %s -> %s\n", mxt_state_name(current_state),
