2888ebc8e60d4989bdd760d3680ea962b3f1a643
Hanumantha Reddy Pothula <c_hpothu@qti.qualcomm.com>
2016-04-27 20:03:38 +0530

    qcacld-2.0: Resolve memory leak issues
    prima to qcacld-2.0 propagation
    
    In SME, if command posting fails then memory allocated for
    command is not freed.
    Free the memory if SME fails to post command.
    
    Change-Id: I281ef5eb9492fe75d639b2bef7ed588aacee8e74
    CRs-Fixed: 974567

--- drivers/staging/qcacld-2.0/CORE/SME/src/sme_common/sme_Api.c
+++ drivers/staging/qcacld-2.0/CORE/SME/src/sme_common/sme_Api.c
@@ -2368,6 +2368,7 @@ eHalStatus sme_SetEseBeaconRequest(tHalHandle hHal, const tANI_U8 sessionId,
    if(status != eHAL_STATUS_SUCCESS)
       pSmeRrmContext->eseBcnReqInProgress = FALSE;
 
+   vos_mem_free(pSmeBcnReportReq);
    return status;
 }
 
@@ -6790,6 +6791,7 @@ eHalStatus smeIssueFastRoamNeighborAPEvent(tHalHandle hHal,
              pUsrCtx = vos_mem_malloc(sizeof(*pUsrCtx));
              if (NULL == pUsrCtx) {
                  smsLog(pMac, LOGE, FL("Memory allocation failed"));
+                 sme_ReleaseGlobalLock( &pMac->sme );
                  return eHAL_STATUS_FAILED_ALLOC;
              }
 
