270d7caa6c3c85ba9b1df348d69f660a711772f5
codeworkx <codeworkx@cyanogenmod.org>
2016-11-20 16:07:54 +0100

    gcc-wrapper: python3 compat
    Change-Id: I23c0c0097b2324d0a0a75a5af9afdf7ba3d0f3c1

--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -55,10 +55,10 @@ ofile = None
 warning_re = re.compile(r'''(.*/|)([^/]+\.[a-z]+:\d+):(\d+:)? warning:''')
 def interpret_warning(line):
     """Decode the message from gcc.  The messages we care about have a filename, and a warning"""
-    line = line.rstrip('\n')
-    m = warning_re.match(line)
+    line = line.rstrip(b'\n')
+    m = warning_re.match(str(line))
     if m and m.group(2) not in allowed_warnings:
-        print "error, forbidden warning:", m.group(2)
+        print("error, forbidden warning:", m.group(2))
 
         # If there is a warning, remove any object if it exists.
         if ofile:
@@ -83,17 +83,17 @@ def run_gcc():
     try:
         proc = subprocess.Popen(args, stderr=subprocess.PIPE)
         for line in proc.stderr:
-            print line,
-            interpret_warning(line)
+            print(line,
+            interpret_warning(line))
 
         result = proc.wait()
     except OSError as e:
         result = e.errno
         if result == errno.ENOENT:
-            print args[0] + ':',e.strerror
-            print 'Is your PATH set correctly?'
+            print(args[0] + ':',e.strerror)
+            print('Is your PATH set correctly?')
         else:
-            print ' '.join(args), str(e)
+            print(' '.join(args), str(e))
 
     return result
 
