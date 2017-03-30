49acd405b3bb03393abe7ac3b1dfff786198755a
mchehab@s-opensource.com <mchehab@s-opensource.com>
2017-03-30 17:11:33 -0300

    kernel-api.rst: fix output of the vsnprintf() documentation
    The vsnprintf() kernel-doc comment uses % character with a special
    meaning other than escaping a constant. As ReST already defines
    ``literal`` as an escape sequence, let's make kernel-doc handle it,
    and use it at lib/vsprintf.c.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>
    Signed-off-by: Jonathan Corbet <corbet@lwn.net>

--- lib/vsprintf.c
+++ lib/vsprintf.c
@@ -1595,13 +1595,13 @@ set_precision(struct printf_spec *spec, int prec)
  * This function generally follows C99 vsnprintf, but has some
  * extensions and a few limitations:
  *
- * %n is unsupported
- * %p* is handled by pointer()
+ *  - ``%n`` is unsupported
+ *  - ``%p*`` is handled by pointer()
  *
  * See pointer() or Documentation/printk-formats.txt for more
  * extensive description.
  *
- * ** Please update the documentation in both places when making changes **
+ * **Please update the documentation in both places when making changes**
  *
  * The return value is the number of characters which would
  * be generated for the given input, excluding the trailing
