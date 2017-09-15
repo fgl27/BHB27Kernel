6e4ab575744416204db5f45686d2bc39f2b2bbc8
Felipe Leon <fglfgl27@gmail.com>
2017-09-15 14:40:23 -0300

    devfreq: Fix store_polling_interval
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -858,6 +858,7 @@ static ssize_t store_polling_interval(struct device *dev,
 	if (ret != 1)
 		return -EINVAL;
 
+	df->profile->polling_ms = value;
 	df->governor->event_handler(df, DEVFREQ_GOV_INTERVAL, &value);
 	ret = count;
 
