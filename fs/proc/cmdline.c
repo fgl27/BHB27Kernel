ea4e2cfcd69db8243d5dcb0c6bdc4224d4571107
Sultanxda <sultanxda@gmail.com>
2017-10-05 21:28:50 -0300

    proc: Remove additional SafetyNet flags from /proc/cmdline
    SafetyNet checks androidboot.veritymode in Nougat, so remove it.
    
    Additionally, remove androidboot.enable_dm_verity and androidboot.secboot
    in case SafetyNet will check them in the future.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    
    add Motorola bootloader related and remove my workarounds
    Test safetynet pass OK
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -24,68 +24,39 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
+static void remove_flag(char *cmd, const char *flag)
+{
+	char *start_addr, *end_addr;
+
+	/* Ensure all instances of a flag are removed */
+	while ((start_addr = strstr(cmd, flag))) {
+		end_addr = strchr(start_addr, ' ');
+		if (end_addr)
+			memmove(start_addr, end_addr + 1, strlen(end_addr));
+		else
+			*(start_addr - 1) = '\0';
+	}
+}
+
+static void remove_safetynet_flags(char *cmd)
+{
+	remove_flag(cmd, "androidboot.flash.locked=");//(this fails safetynet) Motorola bootloader can set this
+	remove_flag(cmd, "androidboot.bl_state=");//Motorola bootloader can set this
+	remove_flag(cmd, "androidboot.verifiedbootstate=");//(this fails safetynet) Google flag used to set others flags
+	remove_flag(cmd, "androidboot.enable_dm_verity=");
+	remove_flag(cmd, "androidboot.secboot=");
+	remove_flag(cmd, "androidboot.veritymode=");
+}
+
 static int __init proc_cmdline_init(void)
 {
-	char *offset_addr, *cmd = new_command_line;
-
-	strcpy(cmd, saved_command_line);
+	strcpy(new_command_line, saved_command_line);
 
 	/*
-	 * Remove 'androidboot.' "bad" or duplicated flags from command line seen
-	 * by userspace in order to pass SafetyNet CTS check.
+	 * Remove various flags from command line seen by userspace in order to
+	 * pass SafetyNet CTS check.
 	 */
-	offset_addr = strstr(cmd, "androidboot.bl_state=2");
-	if (offset_addr) {
-		size_t i, len, offset;
-
-		len = strlen(cmd);
-		offset = offset_addr - cmd;
-		for (i = 1; i < (len - offset); i++) {
-			if (cmd[offset + i] == ' ') {
-				break;
-			}
-		}
-
-		/*
-		 * if ((len - i - offset) == 0) argument is the last and have no space after
-		* when that happens add a space before the argument just to remove it
-		 */
-		if ((len - i - offset) == 0) {
-			offset_addr = strstr(cmd, " androidboot.bl_state=2");
-			len = strlen(cmd);
-			offset = offset_addr - cmd;
-			i = i + 1;
-			memmove(offset_addr, &cmd[offset + i + (i - 3)], i);
-		} else {
-			memmove(offset_addr, &cmd[offset + i + 1], len - i - offset);
-		}
-	}
-
-	offset_addr = strstr(cmd, "androidboot.flash.locked=0");
-	if (offset_addr) {
-		size_t i, len, offset;
-
-		len = strlen(cmd);
-		offset = offset_addr - cmd;
-		for (i = 1; i < (len - offset); i++) {
-			if (cmd[offset + i] == ' ') {
-				break;
-			}
-		}
-		/*
-		 * if ((len - i - offset) == 0) argument is the last and have no space after
-		* when that happens add a space before the argument just to remove it
-		 */
-		if ((len - i - offset) == 0) {
-			offset_addr = strstr(cmd, " androidboot.flash.locked=0");
-			len = strlen(cmd);
-			offset = offset_addr - cmd;
-			i = i + 1;
-			memmove(offset_addr, &cmd[offset + i + (i - 3)], i);
-		} else {
-			memmove(offset_addr, &cmd[offset + i + 1], len - i - offset);
-		}
-	}
+	remove_safetynet_flags(new_command_line);
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
