d565b2da8a3e77b18170356f4f2e9e42be184e93
Felipe Leon <fglfgl27@gmail.com>
2018-06-30 21:11:28 -0300

    Build: drop moduloes checks
    we don't build they anymore
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- build/how_to_build_this.sh
+++ build/how_to_build_this.sh
@@ -116,13 +116,10 @@ if [ -z "$clean" ]; then
 	# Clean temp directory
 	echo -e "\nCleaning temp directory\n";
 	rm -rf ./build/temp
-	rm -rf ./build/bhbkernel/modules/*
 	mkdir ./build/temp
 
 	# Making started
 	make clean && make mrproper
-else
-	rm -rf ./build/bhbkernel/modules/qca_cld/
 fi
 
 if [ ! -d "./build/temp" ]; then
@@ -145,34 +142,12 @@ elif [ ! -e ./build/temp/arch/arm/boot/dt.img.lz4 ]; then
 	grep -B 3 -C 6 -r warn build/build_log.txt
 	exit 1;
 else
-	# moving modules to zip folder
-	cd ./build/temp
-	find  -iname '*.ko' -exec cp -rf '{}' ../bhbkernel/modules/ \;
-	cd -
-	# strip modules 
-	${CROSS_COMPILE}strip --strip-unneeded ./build/bhbkernel/modules/*
-	mkdir ./build/bhbkernel/modules/qca_cld
-	mv ./build/bhbkernel/modules/wlan.ko ./build/bhbkernel/modules/qca_cld/qca_cld_wlan.ko
-fi;
-
-# check if wifi build ok
-if [ ! -e ./build/bhbkernel/modules/qca_cld/qca_cld_wlan.ko ]; then
-	echo -e "$\n{bldred}Wifi module not build check build_log.txt\n"
-	grep -B 3 -C 6 -r error: build/build_log.txt
-	grep -B 3 -C 6 -r warn build/build_log.txt
-	exit 1;
-else
-	cp -rf ./drivers/staging/qcacld-2.0/firmware_bin/WCNSS_qcom_cfg.ini ./build/bhbkernel/system/etc/wifi/WCNSS_qcom_cfg.ini
-	cp -rf ./drivers/staging/qcacld-2.0/firmware_bin/WCNSS_qcom_wlan_nv.bin ./build/bhbkernel/system/etc/wifi/WCNSS_qcom_wlan_nv.bin
-	cp -rf ./drivers/staging/qcacld-2.0/firmware_bin/WCNSS_cfg.dat ./build/bhbkernel/system/etc/firmware/wlan/qca_cld/WCNSS_cfg.dat
 	cp -rf ./build/temp/arch/arm/boot/zImage ./build/bhbkernel/zImage
 	cp -rf ./build/temp/arch/arm/boot/dt.img.lz4 ./build/bhbkernel/dtb
-        rm -rf ./build/*.zip
-        7za a -tzip -r ./build/build.zip ./build/bhbkernel/* '-x!README.md'  '-x!*.gitignore' > /dev/null
-        java -jar ./build/zipsigner.jar './build/build.zip' './build/'$ZIPNAME
-        rm -rf ./build/bhbkernel/system/etc/wifi/WCNSS_qcom_cfg.ini
-        rm -rf ./build/bhbkernel/system/etc/wifi/WCNSS_qcom_wlan_nv.bin
-        rm -rf ./build/bhbkernel/system/etc/firmware/wlan/qca_cld/WCNSS_cfg.dat
+	rm -rf ./build/*.zip
+
+	7za a -tzip -r ./build/build.zip ./build/bhbkernel/* '-x!README.md'  '-x!*.gitignore' > /dev/null
+	java -jar ./build/zipsigner.jar './build/build.zip' './build/'$ZIPNAME
 	echo -e "\nKernel Build OK zip file at... $FOLDER build/bhbkernel/$ZIPNAME \n";
 fi;
 
