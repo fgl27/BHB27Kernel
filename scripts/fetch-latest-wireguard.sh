9e22c8aaf5fa931756f80c610d328bad964759a3
Felipe Leon <fglfgl27@gmail.com>
2019-03-08 14:58:20 -0300

    Scripts update fetch-latest-wireguard.sh
    workaround to much upstrem changes to net
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- scripts/fetch-latest-wireguard.sh
+++ scripts/fetch-latest-wireguard.sh
@@ -15,3 +15,9 @@ rm -rf net/wireguard
 mkdir -p net/wireguard
 curl -A "$USER_AGENT" -LsS "https://git.zx2c4.com/WireGuard/snapshot/WireGuard-${BASH_REMATCH[1]}.tar.xz" | tar -C "net/wireguard" -xJf - --strip-components=2 "WireGuard-${BASH_REMATCH[1]}/src"
 touch net/wireguard/.check
+
+#after https://github.com/fgl27/BHB27Kernel/commit/5c1cc037752bb9e4ca3e12ada879f79c35fc3e30
+#we don't need that workaround
+old="#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 15, 0)"
+new="#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 10, 0)"
+sed  --in-place "s%$old%$new%g" net/wireguard/compat/udp_tunnel/udp_tunnel.c
