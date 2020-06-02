fb449503c98211bb5128eeb4d228878c333b760b
fgl27 <fglfgl27@gmail.com>
2020-06-02 09:36:51 -0300

    fix ASoC: msm: qdsp6v2: fix incorrect packet size calculation
    commit
    https://github.com/fgl27/BHB27Kernel/commit/19d50b84cbf80d0ed48336e8908c7fd3d50c516c
    
    changes the mem_state causing the device to freeze at boot and bootloop
    
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -465,7 +465,7 @@ void send_asm_custom_topology(struct audio_client *ac)
 	}
 
 	q6asm_add_hdr_custom_topology(ac, &asm_top.hdr, sizeof(asm_top), TRUE);
-	atomic_set(&ac->mem_state, -1);
+	atomic_set(&ac->mem_state, 1);
 	asm_top.hdr.opcode = ASM_CMD_ADD_TOPOLOGIES;
 	asm_top.payload_addr_lsw = lower_32_bits(cal_block.cal_paddr);
 	asm_top.payload_addr_msw = upper_32_bits(cal_block.cal_paddr);
